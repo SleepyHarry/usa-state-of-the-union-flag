19fb74b73d02e91431c4ef2df30fe3de95221073
SleepyHarry <harryavane@gmail.com>
2016-03-26 02:11:32 +0000

    bind data

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+var dateFormat = d3.time.format('%B %d %Y');
+
 var margin = {top: 0, right: 0, bottom: 0, left: 0},
     outerWidth = 1235,
     outerHeight = 650,
@@ -43,40 +45,38 @@ var stars = svg.append('g')
 var starXDiff = 247,
     starYDiff = 210;
 
-stars.selectAll('path')
-    .data(Array(50))
-    .enter()
-        .append('path')
-        .attr({
-            d: starD,
-            //x: (d,i) => (i%11) * starXDiff,
-            //y: (d,i) => Math.floor(i/11) * starYDiff,
-            transform: (d,i) => 'translate(' +
-                (((i%11) * 2 * starXDiff) % (11 * starXDiff)) +
-                ',' +
-                (Math.floor(i/5.5) * starYDiff) +
-                ')'
-        });
+d3.csv(
+    'resource/join-dates.csv',
+    function (d) {
+        d.rank = +d.rank;
+        d.date = dateFormat.parse(d.date);
 
+        return d;
+    },
+    function (error, data) {
+        stars.selectAll('path')
+            .data(data)
+            .enter()
+                .append('path')
+                .attr({
+                    d: starD,
+                    //x: (d,i) => (i%11) * starXDiff,
+                    //y: (d,i) => Math.floor(i/11) * starYDiff,
+                    transform: (d,i) => 'translate(' +
+                        (((i%11) * 2 * starXDiff) % (11 * starXDiff)) +
+                        ',' +
+                        (Math.floor(i/5.5) * starYDiff) +
+                        ')'
+                })
+            .on('mouseover', function (d) {
+                d3.select(this).transition()
+                    .attr('fill', '#000');
 
-
-//<g xmlns="http://www.w3.org/2000/svg" id="g3844" transform="translate(0,11.458981)" style="fill:#ffffff">
-//  <g id="s18">
-//      <g id="s9">
-//          <g id="s5">
-//              <g id="s4">
-//                  <path id="s" d="m 247,90 70.53423,217.08204 -184.66101,-134.16408 228.25356,0 -184.66101,134.16408 z" />
-//                  <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#s" y="420" id="use3851" x="0" width="100%" height="100%"/>
-//                  <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#s" y="840" id="use3853" x="0" width="100%" height="100%"/>
-//                  <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#s" y="1260" id="use3855" x="0" width="100%" height="100%"/>
-//              </g>
-//              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#s" y="1680" id="use3857" x="0" width="100%" height="100%"/>
-//          </g>
-//          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#s4" x="247" y="210" id="use3859" width="100%" height="100%"/>
-//      </g>
-//      <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#s9" x="494" id="use3861" y="0" width="100%" height="100%"/>
-//  </g>
-//  <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#s18" x="988" id="use3863" y="0" width="100%" height="100%"/>
-//  <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#s9" x="1976" id="use3865" y="0" width="100%" height="100%"/>
-//  <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#s5" x="2470" id="use3867" y="0" width="100%" height="100%"/>
-//</g>
+                console.log(d);
+            })
+            .on('mouseout', function () {
+                d3.select(this).transition()
+                    .attr('fill', '#fff');
+            });
+    }
+);
